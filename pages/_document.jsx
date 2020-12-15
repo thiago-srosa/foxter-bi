a718cbac7e174f746aae3ed0697ef90e963d4676
thiago-rosa <thiagosrosa@gmail.com>
2020-12-15 00:05:16 -0300

    nothing important

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -35,10 +35,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
-          {/*<meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />}
+          
           {/* PWA primary color */}
           <meta
             name="theme-color"            
