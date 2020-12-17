d5229c8b6b8bd18870267010dcaeb7c753a4422a
thiago-rosa <thiagosrosa@gmail.com>
2020-12-17 15:50:58 -0300

    move files according to nextjs

--- pages/novo-contrato/index.jsx
+++ pages/novo-contrato/index.jsx
@@ -1,16 +1,29 @@
-import useStyles from './styles'
-import TextField from '@material-ui/core/TextField';
+import useStyles from '../../components/test/styles'
 
 import { useState } from 'react';
 import { Auth } from '../../components/Auth';
 
-import SimpleModal from './modalNovoClienteCPF';
+import SimpleModal from '../../components/test/modalNovoClienteCPF';
 
 import 'firebase/auth';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 import Head from 'next/head'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBAPFmFw6QdZkSDlApluWgnEkYtTsHJjSY",
+    authDomain: "foxter-bi-e59ee.firebaseapp.com",
+    projectId: "foxter-bi-e59ee",
+    storageBucket: "foxter-bi-e59ee.appspot.com",
+    messagingSenderId: "202407320050",
+    appId: "1:202407320050:web:592fcd912382725c46736b",
+    measurementId: "G-BHMKVX63H0"
+  })
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 const db = firebase.firestore();
 
