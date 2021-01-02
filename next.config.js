9f1fde5a86a382965833df7950ae98e0f28448f5
thiago-rosa <thiagosrosa@gmail.com>
2021-01-02 19:05:26 -0300

    updating

--- next.config.js
+++ next.config.js
@@ -1,48 +1,15 @@
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-const withOffline = require('next-offline')
 
-module.exports = withOffline(withPWA({
+// change start-url cache strategy, so that we can prompt user to reload when
+// new version available, instead of showing new version directly
+const runtimeCaching = require('next-pwa/cache')
+runtimeCaching[0].handler = 'StaleWhileRevalidate'
+
+module.exports = withPWA({
   pwa: {
     dest: 'public',
-    runtimeCaching,
-  },
-}),
-  {
-    workboxOpts: {
-      runtimeCaching: [
-        {
-          // MUST be the same as "start_url" in manifest.json
-          urlPattern: '/',
-          // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
-          // use StaleWhileRevalidate if you want to prompt user to reload when new version available
-          handler: 'NetworkFirst',
-          options: {
-            // don't change cache name
-            cacheName: 'start-url',
-            expiration: {
-              maxEntries: 1,
-              maxAgeSeconds: 24 * 60 * 60 // 24 hours
-            }
-          }
-        },
-        {
-          urlPattern: /.png$/,
-          handler: 'CacheFirst'
-        },
-        {
-          urlPattern: /api/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheableResponse: {
-              statuses: [0, 200],
-              headers: {
-                'x-test': 'true'
-              }
-            }
-          }
-        }
-      ]
-    }
+    register: false,
+    skipWaiting: false,
+    runtimeCaching
   }
-)
+})
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -76,17 +76,17 @@ function MyApp({ Component, pageProps }) {
         <meta name='theme-color' content='#000000' />
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
         <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://yourdomain.com' />
+        <meta name='twitter:url' content='https://foxter-bi.vercel.app/' />
         <meta name='twitter:title' content='PWA App' />
         <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
+        <meta name='twitter:image' content='https://foxter-bi.vercel.app//static/icons/android-chrome-192x192.png' />
         <meta name='twitter:creator' content='@DavidWShadow' />
         <meta property='og:type' content='website' />
         <meta property='og:title' content='PWA App' />
         <meta property='og:description' content='Best PWA App in the world' />
         <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://yourdomain.com' />
-        <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' />
+        <meta property='og:url' content='https://foxter-bi.vercel.app/' />
+        <meta property='og:image' content='https://foxter-bi.vercel.app//static/icons/apple-touch-icon.png' />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" />
