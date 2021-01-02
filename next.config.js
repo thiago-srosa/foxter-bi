19c8c618605b1065ac2340bd6cabd583a388d3d1
thiago-rosa <thiagosrosa@gmail.com>
2021-01-02 18:21:11 -0300

    another try

--- next.config.js
+++ next.config.js
@@ -1,9 +1,48 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const withOffline = require('next-offline')
 
-module.exports = withPWA({
+module.exports = withOffline(withPWA({
   pwa: {
     dest: 'public',
-    //runtimeCaching,
+    runtimeCaching,
   },
-})
+}),
+  {
+    workboxOpts: {
+      runtimeCaching: [
+        {
+          // MUST be the same as "start_url" in manifest.json
+          urlPattern: '/',
+          // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
+          // use StaleWhileRevalidate if you want to prompt user to reload when new version available
+          handler: 'NetworkFirst',
+          options: {
+            // don't change cache name
+            cacheName: 'start-url',
+            expiration: {
+              maxEntries: 1,
+              maxAgeSeconds: 24 * 60 * 60 // 24 hours
+            }
+          }
+        },
+        {
+          urlPattern: /.png$/,
+          handler: 'CacheFirst'
+        },
+        {
+          urlPattern: /api/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheableResponse: {
+              statuses: [0, 200],
+              headers: {
+                'x-test': 'true'
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+)
