683b5cbe654e762a1a7ea4730f9430516a3d2994
thiago-rosa <thiagosrosa@gmail.com>
2021-01-04 16:41:19 -0300

    new build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+//Import React
+import { useEffect } from 'react'
 
 //import  NextJs
 import Head from 'next/head'
@@ -15,7 +17,6 @@ import 'firebase/auth'
 //import Custom Components
 import CustomDrawer from '../components/Drawer/index'
 
-
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: "AIzaSyBAPFmFw6QdZkSDlApluWgnEkYtTsHJjSY",
@@ -30,7 +31,77 @@ if (!firebase.apps.length) {
   firebase.app(); // if already initialized, use that one
 }
 
+declare const window: any
+
 const MyApp: React.ElementType = ({ Component, pageProps }: AppProps) => {
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
+      const wb = window.workbox
+      // add event listeners to handle any of PWA lifecycle event
+      // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
+      wb.addEventListener('installed', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      wb.addEventListener('controlling', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      wb.addEventListener('activated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+      // NOTE: MUST set skipWaiting to false in next.config.js pwa object
+      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+      const promptNewVersionAvailable = event => {
+        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+        // You may want to customize the UI prompt accordingly.
+
+        wb.addEventListener('install', function (event) {
+          window.location.reload()
+        });
+
+        self.addEventListener('activate', function (event) {
+          window.location.reload()
+        });
+
+      }
+
+      wb.addEventListener('waiting', promptNewVersionAvailable)
+      wb.addEventListener('externalwaiting', promptNewVersionAvailable)
+
+      // ISSUE - this is not working as expected, why?
+      // I could only make message event listenser work when I manually add this listenser into sw.js file
+      wb.addEventListener('message', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      /*
+      wb.addEventListener('redundant', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      wb.addEventListener('externalinstalled', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      wb.addEventListener('externalactivated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      */
+
+      // never forget to call register as auto register is turned off in next.config.js
+      wb.register()
+    }
+  }, [])
 
   return (
     <>
