0c1350493489af9bbc0992d146edc90f0d38e702
thiago-rosa <thiagosrosa@gmail.com>
2021-01-04 09:54:11 -0300

    force app update

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,18 +54,8 @@ export default function Home(): React.ReactElement {
         // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
         // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
-        if (confirm('A newer version of this web app is available, reload to update?')) {
-          wb.addEventListener('controlling', event => {
-            window.location.reload()
-          })
-
-          // Send a message to the waiting service worker, instructing it to activate.
-          wb.messageSW({ type: 'SKIP_WAITING' })
-        } else {
-          console.log(
-            'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
-          )
-        }
+        alert("Atualizando aplicativo...")
+        window.location.reload()
       }
 
       wb.addEventListener('waiting', promptNewVersionAvailable)
